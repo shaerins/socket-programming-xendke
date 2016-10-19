023ff9ebd5c51960aaef898a45ed7fd2b982c011
xendke <xavier96@hotmail.it>
2016-10-19 11:30:48 +0000

    filename, serving file

--- simple_server.py
+++ simple_server.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 2. "Bind" the socket to an IP and PORT
-my_socket.bind(("localhost", 3000))
+my_socket.bind(("localhost", 3001))
 # 3. Begin "listening" on the socket
 my_socket.listen(5)
 
@@ -18,7 +18,7 @@ conn, addr = my_socket.accept()
 def openFile(filename):
     return open(filename, "rb").read()
 
-htmlf = open("inx.html", "rb").read()
+index_html = open("index.html", "rb").read()
 
 # 5. Receive some data (up to 1024 bytes) FROM the client
 data = ''
@@ -35,6 +35,15 @@ for i in range(0, len(data_string)):
         break
 
 # TODO: loop through get_string to find filename and serve, filename
+filename = ""
+for i in range(4, len(get_string)):
+    if(get_string[i] == " "):
+        filename = get_string[5:i]
+        print(filename)
+        break
 
 if(get_string[0:3] == "GET"):
-    conn.sendall(htmlf)
+    if(not filename):
+        conn.sendall(index_html)
+    else:
+        conn.sendall(openFile(filename))
