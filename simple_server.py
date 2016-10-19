abeb7e3b05de16c9f7b0cf79237b9b314f275cc6
xendke <xavier96@hotmail.it>
2016-10-19 11:46:00 +0000

    404

--- simple_server.py
+++ simple_server.py
@@ -4,46 +4,48 @@ import socket
 if __name__ == "__main__":
     print("Hello World!")
 
-# 1. Create a socket
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 1. Create a socket
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 2. "Bind" the socket to an IP and PORT
+    my_socket.bind(("localhost", 3001))
+    # 3. Begin "listening" on the socket
+    my_socket.listen(5)
+    # 4. Begin "accepting" client connections
+    conn, addr = my_socket.accept()
 
-# 2. "Bind" the socket to an IP and PORT
-my_socket.bind(("localhost", 3001))
-# 3. Begin "listening" on the socket
-my_socket.listen(5)
+    def openFile(filename):
+        return open(filename, "rb").read()
 
-# 4. Begin "accepting" client connections
-conn, addr = my_socket.accept()
+    index_html = open("index.html", "rb").read()
+    four_oh_four = open("404.html", "rb").read()
 
-def openFile(filename):
-    return open(filename, "rb").read()
+    # 5. Receive some data (up to 1024 bytes) FROM the client
+    data = ''
+    data = conn.recv(1024)
+    print(data)
 
-index_html = open("index.html", "rb").read()
+    data_string = str(data)
+    get_string = ""
 
-# 5. Receive some data (up to 1024 bytes) FROM the client
-data = ''
-data = conn.recv(1024)
-print(data)
+    for i in range(0, len(data_string)):
+        if(data_string[i:i+4] == "\\r\\n"):
+            get_string = data_string[2:i]
+            print(get_string)
+            break
 
-data_string = str(data)
-get_string = ""
+    if(get_string[0:3] == "GET"):
+        filename = ""
+        for i in range(4, len(get_string)):
+            if(get_string[i] == " "):
+                filename = get_string[5:i]
+                print(filename)
+                break
 
-for i in range(0, len(data_string)):
-    if(data_string[i:i+4] == "\\r\\n"):
-        get_string = data_string[2:i]
-        print(get_string)
-        break
-
-# TODO: loop through get_string to find filename and serve, filename
-filename = ""
-for i in range(4, len(get_string)):
-    if(get_string[i] == " "):
-        filename = get_string[5:i]
-        print(filename)
-        break
-
-if(get_string[0:3] == "GET"):
-    if(not filename):
-        conn.sendall(index_html)
-    else:
-        conn.sendall(openFile(filename))
+        if(not filename):
+            conn.sendall(index_html)
+        else:
+            try:
+                conn.sendall(openFile(filename))
+            except FileNotFoundError:
+                conn.sendall(four_oh_four)
+	    
